f490957aa4211cbcc76b724e65bd726e198bb780
Yurii Postevka <yurapostevka@gmail.com>
2020-11-20 14:03:27 +0200

    quality rtc webhook. parse req to cloud event.

--- functions/qualityrtc-slack-message/index.js
+++ functions/qualityrtc-slack-message/index.js
@@ -1,27 +1,37 @@
 const { sendSlackMessage } = require("../../slack-message");
+const { HTTP } = require("cloudevents");
 
 exports.handler = async (event, context) => {
+  const receivedCloudEvent = HTTP.toEvent({
+    headers: event.headers,
+    body: event.body,
+  });
+
   let message = "";
   let isSuccess = true;
   try {
-    const data = JSON.parse(event.body);
     isSuccess = true;
     message = `
     Cloud Event Slack Example:
     ============================
     Incoming data
-      ${JSON.stringify(data, null, 2)}
+      ${JSON.stringify(receivedCloudEvent.data, null, 2)}
     ============================
     `;
   } catch (err) {
-    message = `Error in qualityrtc-slack-message-director, failed to parse incoming webhook body [${event.body}]`;
+    message = `Error in qualityrtc-slack-message, cloud event[${{
+      data: receivedCloudEvent.data,
+      source: receivedCloudEvent.source,
+      type: receivedCloudEvent.type,
+    }}]`;
     isSuccess = false;
+    console.log(err.message, err.stack);
   }
 
   const webHookURL = process.env.SLACK_QUALITYRTC_WEBHOOK_URL;
 
   await sendSlackMessage(webHookURL, {
-    username: "qualityrtc-slack-message-director",
+    username: "qualityrtc-slack-message",
     icon_emoji: isSuccess ? ":recycle:" : ":bangbang:",
     text: message,
   });
